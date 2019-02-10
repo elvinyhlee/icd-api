cca61c58053c72401cfcfa8b6940447bf4ef92ff
elvinyhlee <elvin.lee@bowtie.com.hk>
2019-02-10 10:17:39 +0800

    increase memory limit

--- app.js
+++ app.js
@@ -59,5 +59,7 @@ app.get('/suggestion/:query', (req, res) => {
 //   },
 //   graphiql: true,
 // }));
+const used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${used} MB`);
 
 app.listen(PORT);
